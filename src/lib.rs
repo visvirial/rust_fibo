9e34be6c4141cd0d481756c4b294169bdff88a2b
Vis Virial <visvirial@gmail.com>
2021-06-03 06:43:42 +0900

    Fix tests.

--- src/lib.rs
+++ src/lib.rs
@@ -54,19 +54,19 @@ pub fn fibo_mat_loop<T: Index + Clone, U: EuclideanDomain + Clone>(n: T, m: U) -
 mod tests {
     use super::*;
     #[test]
-    fn fibo_rec_n_10_u32() {
+    fn fibo_rec_10_1000_u32() {
         assert_eq!(fibo_rec(10, 1000), 55);
     }
     #[test]
-    fn fibo_rec_n_10_big_uint() {
+    fn fibo_rec_10_1000_big_uint() {
         assert_eq!(fibo_rec(BigUint::from(10u32), BigUint::from(1000u32)), BigUint::from(55u32));
     }
     #[test]
-    fn fibo_seq_n_10_u32() {
+    fn fibo_seq_1000_1000000000_u32() {
         assert_eq!(fibo_seq(1000, 1000_000_000), 849228875);
     }
     #[test]
-    fn fibo_seq_n_10_big_uint() {
+    fn fibo_seq_1000_1000000000_big_uint() {
         assert_eq!(fibo_seq(BigUint::from(1000u32), BigUint::from(1000_000_000u32)), BigUint::from(849228875u32));
     }
     #[test]
@@ -75,7 +75,7 @@ mod tests {
     }
     #[test]
     fn fibo_mat_1000000_1000_big_uint() {
-        assert_eq!(fibo_mat(BigUint::from(1000_000u32), BigUint::from(1000u32)), BigUint::from(875u32));
+        assert_eq!(fibo_mat(BigUint::from(1000u32), BigUint::from(1000_000_000u32)), BigUint::from(849228875u32));
     }
     #[test]
     fn fibo_mat_req_1000000_1000_u32() {
