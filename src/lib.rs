caac9b0135ea743b3ef30ea3f7a12dfe2c1d931b
Vis Virial <visvirial@gmail.com>
2021-06-03 07:55:17 +0900

    Add comments.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,11 @@ pub trait Index: EuclideanDomain + Exponent {}
 impl Index for u64 {}
 impl Index for BigUint {}
 
+/// Compute the n-th Fibonacci number modulo m using recurssion (F(n) <= F(n-1) + F(n-2)).
+/// Since the recurrsion computes the same Fibonacci numbers multiple times,
+/// it is very slow to compute.
+/// The computation time is O(F(n)) = O(φ^n).
+/// Maybe n = ~40 is the maximum value that this function can compute the Fibonacci number.
 pub fn fibo_rec<T: Index + Clone>(n: T, m: T) -> T {
     if n.is_zero() {
         return T::zero();
@@ -19,6 +24,8 @@ pub fn fibo_rec<T: Index + Clone>(n: T, m: T) -> T {
     (fibo_rec(n.clone() - T::one() - T::one(), m.clone()) + fibo_rec(n - T::one(), m.clone())) % m
 }
 
+/// Compute the n-th Fibonacci number modulo m using sequencial computation ((F(n), F(n-1)) <= (F(n-1) + F(n-2), F(n-1))).
+/// The computation time is O(n).
 pub fn fibo_seq<T: Index + Clone>(n: T, m: T) -> T {
     let mut a = T::zero();
     let mut b = T::one();
@@ -32,6 +39,9 @@ pub fn fibo_seq<T: Index + Clone>(n: T, m: T) -> T {
     a
 }
 
+/// Compute the n-th Fibonacci number modulo m using a Q-matrix with sequential matrix
+/// multiplication.
+/// The computation time is O(n).
 pub fn fibo_mat<T: Index + Clone>(n: T, m: T) -> T {
     let mut t = mat::Mat::one();
     let mut i = T::zero();
@@ -42,10 +52,17 @@ pub fn fibo_mat<T: Index + Clone>(n: T, m: T) -> T {
     t.0.1
 }
 
+/// Compute the n-th Fibonacci number modulo m using a Q-matrix with repeated squaring
+/// (using recurrsion).
+/// The computation time is O(log(n)).
 pub fn fibo_mat_req<T: Index + Clone, U: EuclideanDomain + Clone>(n: T, m: U) -> U {
     mat::exp_req(&mat::Mat::q_matrix(), n, m).0.1
 }
 
+/// Compute the n-th Fibonacci number modulo m using a Q-matrix with repeated squaring
+/// (using loops).
+/// Slightly faster than `fibo_mat_req()` since there are no function call overhead.
+/// The computation time is O(log(n)).
 pub fn fibo_mat_loop<T: Index + Clone, U: EuclideanDomain + Clone>(n: T, m: U) -> U {
     mat::Mat::q_matrix().pow(n, m).0.1
 }
